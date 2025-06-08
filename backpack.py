36edbe2cd5f903d0317d9cf7a80dd2b312f7eadd
Krzysztof Makulewski <knmekowski@gmail.com>
2025-06-08 12:08:01 +0200

    Changed BackpackBrute

--- backpack.py
+++ backpack.py
@@ -1,4 +1,4 @@
-def BackpackBrute(Capacity: int, Values: list[int], Volume: list[int], Amount: int) -> tuple[int,list[tuple[int,int]]]:
+def BackpackBrute(Capacity: int, Values: list[int], Volume: list[int]) -> tuple[int,list[tuple[int,int]]]:
     """
     This function solves the backpack/knapsack problem using the brute force method
     Maximizes the value of items that can be put into the backpack without going over backpacks capacity
@@ -18,35 +18,36 @@ def BackpackBrute(Capacity: int, Values: list[int], Volume: list[int], Amount: i
 
     if len(Values) != len(Volume):
         raise ValueError("Values and Volume must be the same length")
-
-    if Amount > len(Values):
-        raise ValueError("Amount cannot be grater then length of Values or Volume")
     
-    if Amount < 0 or Capacity < 0:
-        raise ValueError("Amount and capacity cannot be negative")
-
-    # Checked all items or filled the backpack
-    if Amount==0 or Capacity ==0:
-        return (0,[])
+    if Capacity < 0:
+        raise ValueError("Capacity cannot be negative")
     
-    pick = 0
-    pick_items = []
+    # Function that recursively checks every option
+    def BackpackBruteRec(Capacity: int, Values: list[int], Volume: list[int], Amount: int) -> tuple[int,list[tuple[int,int]]]:
+        # Checked all items or filled the backpack
+        if Amount==0 or Capacity ==0:
+            return (0,[])
+        
+        pick = 0
+        pick_items = []
 
-    # Pick n-1 item
-    if Volume[Amount-1] <= Capacity:
-        pick, pick_items = BackpackBrute(Capacity - Volume[Amount-1], Values, Volume, Amount-1)
-        pick += Values[Amount-1]
-        pick_items.append((Values[Amount-1],Volume[Amount-1]))
+        # Pick n-1 item
+        if Volume[Amount-1] <= Capacity:
+            pick, pick_items = BackpackBrute(Capacity - Volume[Amount-1], Values, Volume, Amount-1)
+            pick += Values[Amount-1]
+            pick_items.append((Values[Amount-1],Volume[Amount-1]))
 
-    # Don't pick n-1 item
-    noPick, noPick_items = BackpackBrute(Capacity, Values, Volume, Amount-1)
+        # Don't pick n-1 item
+        noPick, noPick_items = BackpackBrute(Capacity, Values, Volume, Amount-1)
 
-    if pick >= noPick:
-        return (pick,pick_items)
-    else:
-        return (noPick,noPick_items)
+        if pick >= noPick:
+            return (pick,pick_items)
+        else:
+            return (noPick,noPick_items)
     
+    return BackpackBruteRec(Capacity, Values, Volume, len(Values))
+
 
 if __name__ == "__main__":
-    print(BackpackBrute(5, [1,2,3,4,5], [5,4,3,2,1], 5))
+    print(BackpackBrute(5, [1,2,3,4,5], [5,4,3,2,1]))
     
