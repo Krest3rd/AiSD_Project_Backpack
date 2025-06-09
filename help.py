7c7bd70f63f39e378a017d82af1408b709c4d2ba
NdKr <knightyleaf@gmail.com>
2025-06-09 12:02:16 +0200

    added decorations to help.py

--- help.py
+++ help.py
@@ -1,16 +1,18 @@
 from time import process_time
 from collections.abc import Callable
+from decorations import header,line
 
 def printSolution(Solution: tuple[int,list[tuple[int,int]]]) -> None:
     """
     Prints out the solution to Knapsack problem as returned by functions in backpack.py
     """
 
-    print("Value\tVolume\n--------------")
+    print("Value\tVolume")
+    line("-",length=15)
     for i in Solution[1]:
         print(i[0],"\t",i[1])
-    print("--------------")
-    print("Maximum value that can be carried: ", Solution[0])
+    line("-",length=15)
+    header(f"Maximum value that can be carried: {Solution[0]}",pattern="-",length=50)
 
 def benchmark(func: Callable, args: tuple) -> None:
 
@@ -23,4 +25,4 @@ def benchmark(func: Callable, args: tuple) -> None:
     return end - start
 
 if __name__=="__main__":
-    print("Execution time: ",benchmark(printSolution,(1249,[(14,2), (1235,1)])))
+    header(f"Execution time: {benchmark(printSolution,(1249,[(14,2), (1235,1)]))}",pattern="-",length=50)
