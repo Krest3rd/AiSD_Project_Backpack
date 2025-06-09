4a54cb12033833d940263eab1ef16ff8b47d741f
NdKr <knightyleaf@gmail.com>
2025-06-09 11:48:38 +0200

    added invalid

---
+++ invalid.py
@@ -0,0 +1,19 @@
+import sys
+from decoration import header, footer, line, print_text_multiline
+
+def invalid_usage():
+    header("Correct usage: ./plik np. ./main.py", pattern="-",length=60)
+    sys.exit(1)
+
+def invalid_command():
+    header("INVALID COMMAND", pattern="-", length=43)
+    print_text_multiline("Please enter a valid command or type 'help' for assistance.", width=43)
+    footer("-",length=43)
+
+def invalid_eof():
+    header("Ctrl+D detected. Exiting.", pattern="-")
+    sys.exit(1)
+    
+def invalid_keyboard_interrupt():
+    header("Ctrl+C detected. Exiting.", pattern="-")
+    sys.exit(0)
